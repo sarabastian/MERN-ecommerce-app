0604d7fcf1ff1e668e3b1fb7438e4eac8230a78e
sarabastian <seb118@georgetown.edu>
2021-03-22 21:23:17 -0400

    custom authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { authUser } from "../controllers/userController.js";
+import { authUser, getUserProfile } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 router.post("/login", authUser);
+router.route("/profile").get(protect, getUserProfile);
 
 // @desc    Fetch single product
 // @route   GET /api/product/:id
